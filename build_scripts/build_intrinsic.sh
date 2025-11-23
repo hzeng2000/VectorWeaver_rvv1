7bf927f4f819ff2a029a3e728f3ed4cf31676866
hzeng2000 <hzeng@stu.xidian.edu.cn>
2025-11-23 11:19:50 +0800

    bug-fix for ggml_vec_dot_q4_0_q8_0

---
+++ build_scripts/build_intrinsic.sh
@@ -0,0 +1,3 @@
+rm -rf *
+cmake -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_SYSTEM_PROCESSOR=riscv64 -DCMAKE_C_COMPILER=riscv64-unknown-linux-gnu-gcc -DCMAKE_CXX_COMPILER=riscv64-unknown-linux-gnu-g++ -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="-march=rv64gc_xtheadvector1p0_zfhmin" -DCMAKE_CXX_FLAGS="-march=rv64gc_xtheadvector1p0_zfhmin" -DLLAMA_CURL=OFF -DGGML_XTHEADVECTOR=True ..
+make -j96
