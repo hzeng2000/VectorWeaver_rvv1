e8cadcf14e3809c7cee7e490224e027d54b21469
hzeng2000 <hzeng@stu.xidian.edu.cn>
2025-12-14 23:34:03 +0800

    9

--- ggml/src/ggml-cpu/arch/riscv/quants.c
+++ ggml/src/ggml-cpu/arch/riscv/quants.c
@@ -470,7 +470,7 @@ static inline void process_single_block_asm_ggml_vec_dot_q8_0_q8_0_asm_unroll2_(
 }
 
 // Helper function for tail loop - RVV 1.0 版本
-static inline void process_single_block_asm_ggml_vec_dot_q8_0_q8_0_asm_unroll2(const block_q8_0* x, const block_q8_0* y, float* sumf) {
+static inline void process_single_block_asm_ggml_vec_dot_q8_0_q8_0_asm_unroll2_prefetch1(const block_q8_0* x, const block_q8_0* y, float* sumf) {
     int32_t sumi;
     asm volatile(
         "li t0, 32\n\t"
@@ -533,12 +533,15 @@ void ggml_vec_dot_q8_0_q8_0(int n, float * GGML_RESTRICT s, size_t bs, const voi
 
     *s = sumf;
 #elif defined(__RVV_ASM_STD)
-    for (; ib + 1 < nb; ib += 2) {
+    for (; ib + 2 < nb; ib += 2) {
         int32_t sumi[2];
         asm volatile(
             "li t0, 32\n\t"
             /******************* CODE BLOCK START *******************/
 
+"vsetvli x0, t0, e8, m2, ta, ma\n\t"
+            "vle8.v v26, (%[x_pf0_ptr])\n\t"
+            "vle8.v v28, (%[y_pf0_ptr])\n\t"
 
             // Block 0
             "vsetvli x0, t0, e8, m2, ta, ma\n\t"
@@ -567,7 +570,7 @@ void ggml_vec_dot_q8_0_q8_0(int n, float * GGML_RESTRICT s, size_t bs, const voi
             "vmv.x.s %[sumi0], v24\n\t"
             "vmv.x.s %[sumi1], v25\n\t"
             /******************** CODE BLOCK END ********************/
-            :  [sumi0] "=r"(sumi[0]),  [sumi1] "=r"(sumi[1])             :  [x0_ptr] "r"(x[ib+0].qs), [y0_ptr] "r"(y[ib+0].qs),  [x1_ptr] "r"(x[ib+1].qs), [y1_ptr] "r"(y[ib+1].qs)             :               "t0", "memory", "v8", "v9", "v10", "v11", "v12", "v13", "v14", "v15", "v16", "v17", "v18", "v19", "v20", "v21", "v22", "v23", "v24", "v25");
+            :  [sumi0] "=r"(sumi[0]),  [sumi1] "=r"(sumi[1])             :  [x0_ptr] "r"(x[ib+0].qs), [y0_ptr] "r"(y[ib+0].qs),  [x1_ptr] "r"(x[ib+1].qs), [y1_ptr] "r"(y[ib+1].qs),  [x_pf0_ptr] "r"(x[ib+2].qs), [y_pf0_ptr] "r"(y[ib+2].qs)             :               "t0", "memory", "v8", "v9", "v10", "v11", "v12", "v13", "v14", "v15", "v16", "v17", "v18", "v19", "v20", "v21", "v22", "v23", "v24", "v25", "v26", "v27", "v28", "v29");
         
         sumf += (float)sumi[0] * GGML_CPU_FP16_TO_FP32(x[ib+0].d) * GGML_CPU_FP16_TO_FP32(y[ib+0].d);
         sumf += (float)sumi[1] * GGML_CPU_FP16_TO_FP32(x[ib+1].d) * GGML_CPU_FP16_TO_FP32(y[ib+1].d);
@@ -575,7 +578,7 @@ void ggml_vec_dot_q8_0_q8_0(int n, float * GGML_RESTRICT s, size_t bs, const voi
 
     // Tail loop
     for (; ib < nb; ++ib) {
-        process_single_block_asm_ggml_vec_dot_q8_0_q8_0_asm_unroll2(&x[ib], &y[ib], &sumf);
+        process_single_block_asm_ggml_vec_dot_q8_0_q8_0_asm_unroll2_prefetch1(&x[ib], &y[ib], &sumf);
     }
     *s = sumf;
 #else
