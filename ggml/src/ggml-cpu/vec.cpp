01c34291e4565163c1a438f7d701160e9f47751f
hzeng2000 <hzeng@stu.xidian.edu.cn>
2025-12-20 10:48:17 +0800

    4

--- ggml/src/ggml-cpu/vec.cpp
+++ ggml/src/ggml-cpu/vec.cpp
@@ -258,7 +258,7 @@ void ggml_vec_silu_f32(const int n, float * y, const float * x) {
 }
 
 // Helper for tail processing - RVV 1.0 版本
-static inline void process_swiglu_tail_asm_fast_ggml_vec_swiglu_f32_asm_unroll2(float* y, const float* x, const float* g, size_t vl) {
+static inline void process_swiglu_tail_asm_fast_ggml_vec_swiglu_f32_asm_unroll2_interleaved(float* y, const float* x, const float* g, size_t vl) {
     const float exp_alpha_f = 12102203.0f;
     const int32_t exp_bias_i  = 1065353216;
     const float one_f = 1.0f;
@@ -322,14 +322,19 @@ void ggml_vec_swiglu_f32(const int n, float * y, const float * x, const float * 
         if (can_unroll) {
             size_t vl_bytes = vl * 4;
             asm volatile (
-                    // --- Load one chunk (x and g) ---
+                    // --- Load all chunks (x and g) ---
                     "vsetvli x0, %[vl], e32, m1, ta, ma\n\t"
                     "vle32.v v8, (%[x_ptr])\n\t"
                     "vle32.v v11, (%[g_ptr])\n\t"
                     "add %[x_ptr], %[x_ptr], %[vl_bytes]\n\t"
                     "add %[g_ptr], %[g_ptr], %[vl_bytes]\n\t"
-
-                    // --- Compute silu for one chunk ---
+                    "vsetvli x0, %[vl], e32, m1, ta, ma\n\t"
+                    "vle32.v v12, (%[x_ptr])\n\t"
+                    "vle32.v v15, (%[g_ptr])\n\t"
+                    "add %[x_ptr], %[x_ptr], %[vl_bytes]\n\t"
+                    "add %[g_ptr], %[g_ptr], %[vl_bytes]\n\t"
+                    
+                    // --- Compute silu for all chunks ---
                     "vsetvli x0, %[vl], e32, m1, ta, ma\n\t"
                     "vfneg.v v9, v8\n\t"
                     "vfmax.vf v9, v9, %[clamp_min]\n\t"
@@ -341,20 +346,6 @@ void ggml_vec_swiglu_f32(const int n, float * y, const float * x, const float * 
                     "vfdiv.vv v8, v8, v9\n\t"
                     // Multiply by gate
                     "vfmul.vv v8, v8, v11\n\t"
-
-                    // --- Store one chunk ---
-                    "vsetvli x0, %[vl], e32, m1, ta, ma\n\t"
-                    "vse32.v v8, (%[y_ptr])\n\t"
-                    "add %[y_ptr], %[y_ptr], %[vl_bytes]\n\t"
-
-                    // --- Load one chunk (x and g) ---
-                    "vsetvli x0, %[vl], e32, m1, ta, ma\n\t"
-                    "vle32.v v12, (%[x_ptr])\n\t"
-                    "vle32.v v15, (%[g_ptr])\n\t"
-                    "add %[x_ptr], %[x_ptr], %[vl_bytes]\n\t"
-                    "add %[g_ptr], %[g_ptr], %[vl_bytes]\n\t"
-
-                    // --- Compute silu for one chunk ---
                     "vsetvli x0, %[vl], e32, m1, ta, ma\n\t"
                     "vfneg.v v13, v12\n\t"
                     "vfmax.vf v13, v13, %[clamp_min]\n\t"
@@ -366,12 +357,14 @@ void ggml_vec_swiglu_f32(const int n, float * y, const float * x, const float * 
                     "vfdiv.vv v12, v12, v13\n\t"
                     // Multiply by gate
                     "vfmul.vv v12, v12, v15\n\t"
-
-                    // --- Store one chunk ---
+                    
+                    // --- Store all chunks ---
+                    "vsetvli x0, %[vl], e32, m1, ta, ma\n\t"
+                    "vse32.v v8, (%[y_ptr])\n\t"
+                    "add %[y_ptr], %[y_ptr], %[vl_bytes]\n\t"
                     "vsetvli x0, %[vl], e32, m1, ta, ma\n\t"
                     "vse32.v v12, (%[y_ptr])\n\t"
                     "add %[y_ptr], %[y_ptr], %[vl_bytes]\n\t"
-
 
                 : [x_ptr] "+r"(x), [g_ptr] "+r"(g), [y_ptr] "+r"(y)
                 : [vl] "r"(vl), [vl_bytes] "r"(vl_bytes),
@@ -381,7 +374,7 @@ void ggml_vec_swiglu_f32(const int n, float * y, const float * x, const float * 
             i += vl * 2;
         } else {
             // Process remaining tail or single chunk
-            process_swiglu_tail_asm_fast_ggml_vec_swiglu_f32_asm_unroll2(y + i, x + i, g + i, vl);
+            process_swiglu_tail_asm_fast_ggml_vec_swiglu_f32_asm_unroll2_interleaved(y + i, x + i, g + i, vl);
             i += vl;
         }
     }
